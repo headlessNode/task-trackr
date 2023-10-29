c03fadad2a330a01d1a53a6628c9bc7adec825e5
headlessNode <harriskhan047@outlook.com>
2023-10-29 19:14:42 +0500

    Complete footer design - Add css styling to footer. - Add add button. - Add menu button. - Add font awesome cdn link for icons.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './style.css'
 
 const content = document.querySelector('.content');
+
+const footer = document.createElement('div');
+footer.classList.add('footer');
+
+const menuBtn = document.createElement('button');
+menuBtn.classList.add('menu-btn');
+menuBtn.innerHTML = '<i class="fa-solid fa-bars fa-xl" style="color: #dce3e4;"></i>';
+
+
+
+const emptyContainer = document.createElement('div');
+emptyContainer.classList.add('empty-container');
+
+
+
+const addTaskBtn = document.createElement('button');
+addTaskBtn.classList.add('add-task-btn');
+addTaskBtn.innerHTML = '<i class="fa-solid fa-plus fa-lg" style="color: #dce3e4;"></i>';
+
+footer.appendChild(emptyContainer);
+footer.appendChild(addTaskBtn);
+footer.appendChild(menuBtn);
+
+content.appendChild(footer);
