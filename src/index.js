0261ea4f92f707fcbfe2cdad604399cb2fe9ef4d
headlessNode <harriskhan047@outlook.com>
2023-10-29 20:52:32 +0500

    Add initial logic for side menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './styles/style.css'
 
 const content = document.querySelector('.content');
 
+//mainbody
+const mainBody = document.createElement('div');
+mainBody.classList.add('main-body');
+mainBody.textContent = 'MAIN BODY';
+
+//Footer
 const footer = document.createElement('div');
 footer.classList.add('footer');
 
@@ -10,7 +16,7 @@ menuBtn.classList.add('menu-btn');
 menuBtn.innerHTML = '<i class="fa-solid fa-bars fa-xl" style="color: #dce3e4;"></i>';
 
 
-
+//for styling
 const emptyContainer = document.createElement('div');
 emptyContainer.classList.add('empty-container');
 
@@ -24,4 +30,26 @@ footer.appendChild(emptyContainer);
 footer.appendChild(addTaskBtn);
 footer.appendChild(menuBtn);
 
+//sidemenu
+const sideMenu = document.createElement('div');
+sideMenu.classList.add('sidebar');
+sideMenu.innerHTML = '<div class="inbox">Inbox</div><div class="today">Today</div><div class="this-week">This week</div>';
+
+content.appendChild(mainBody);
 content.appendChild(footer);
+content.appendChild(sideMenu);
+
+menuBtn.addEventListener('click', ()=>{
+    if(sideMenu.style.display === 'none' || sideMenu.style.display === ''){
+        sideMenu.style.display = 'grid';
+        sideMenu.style.gridColumn = '1/2';
+        sideMenu.style.gridRow = '2/4';
+        mainBody.style.gridColumn = '2/3';
+        footer.style.gridColumn = '2/3';
+    }
+    else{
+        sideMenu.style.display = 'none'
+        mainBody.style.gridColumn = '1/3';
+        footer.style.gridColumn = '1/3';
+    }
+});
